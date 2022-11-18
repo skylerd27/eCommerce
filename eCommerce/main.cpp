e7eada08d0bfaf3d2416362d7ddfd1c083203045
Skyler Dale <skyderp27@gmail.com>
2022-11-18 13:40:17 -0600

    This is the first commit to the remote repo. Just to see if it works.

--- eCommerce/main.cpp
+++ eCommerce/main.cpp
@@ -6,9 +6,10 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
+    cout << "This is a test!" << endl;
     return 0;
 }
